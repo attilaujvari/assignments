6d1c40a533c74632d99f2f7113134d0af5640c23
Attila Ujvari <attila.ujvari2@gmail.com>
2019-02-13 22:23:09 -0700

    beg proj todo2

--- projects/axios-todo/app.js
+++ projects/axios-todo/app.js
@@ -3,12 +3,19 @@ var imageEdit = document.createElement("img");
 imageEdit.src = "./icon_edit.png";
 var imageDelete = document.createElement("img");
 imageDelete.src = "./icon_delete.png";
-// Date function and population to HTML
+// Date function and population to HTML /////////////
 var date = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//yesterday
+document.getElementById("yesterdayDay").innerHTML = days[date.getDay()-1];
+document.getElementById("yesterdayDate").innerHTML = months[date.getMonth()] + " " + (date.getDate()-1);
+//today
 document.getElementById("todayDay").innerHTML = days[date.getDay()];
 document.getElementById("todayDate").innerHTML = months[date.getMonth()] + " " + date.getDate();
+//tomorrow
+document.getElementById("tomorrowDay").innerHTML = days[date.getDay()+1];
+document.getElementById("tomorrowDate").innerHTML = months[date.getMonth()] + " " + (date.getDate()+1);
 
 // Dynamic greeting (based on time, name later) ////////////////////////////
 var curHr = date.getHours();
@@ -23,7 +30,7 @@ if (curHr >= 0 && curHr < 6) {
 }
 
 
-var todoList = document.getElementById('todo-item-list')
+var todoList = document.getElementsByClassName('todo-item-list')[0];
 axios.get("https://api.vschool.io/attila/todo").then(function(response){
     var todos = response.data;
     listTodos(todos)
@@ -36,7 +43,7 @@ function listTodos(todos){
         var title = document.createElement('li');
         title.textContent = todos[i].title;
         todoList.appendChild(title);
-        todoList.appendChild(imageEdit).className = "icon";
-        todoList.appendChild(imageDelete).className = "icon"
+        todoList.appendChild(imageEdit).className = "iconEdit";
+        todoList.appendChild(imageDelete).className = "iconDelete"
     }
 }
