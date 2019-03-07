84e1997cd6b3bc878e015d409c31bb264c90dbe2
Attila Ujvari <attila.ujvari2@gmail.com>
2019-03-06 17:11:51 -0700

    some

---
+++ exercises/adjacent-difference/App.js
@@ -0,0 +1,9 @@
+function combined(arr){
+    // Find the 3 adjacent elements with longest combined length
+    let longestArr = []
+    let word1, word2, word3 = ""
+    
+}
+
+console.log(combined(["this", "is", "an", "array"]))
+// Output:  ["is", "an", "array"]
